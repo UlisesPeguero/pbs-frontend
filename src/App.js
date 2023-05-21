0046d18bbd7e6913519a345b720f90df6fc5922d
Ulises Fox Peguero <peguero.ulises@gmail.com>
2023-05-20 20:01:25 -0500

    Revert "Created AppBar and basic User Profile Picture functionality"

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import { Box, Container, ThemeProvider, createTheme } from '@mui/material';
-import PBSAppBar from './home/PBSAppBar.jsx';
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
 
 function App() {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <PBSAppBar />
-      <Container maxWidth={false} disableGutters={true}>
-        <Box sx={{ backgroundColor: 'primary.dark' }} />
-      </Container>
-    </ThemeProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
